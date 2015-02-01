7bf73e381101aa7fac702d2831d09438eb077b49
edgarmoises <edgar.mope.os@gmail.com>
2015-02-01 01:00:41 -0600

    Se agrega un método

--- src/PruebaGit.java
+++ src/PruebaGit.java
@@ -2,8 +2,20 @@
  * Created by Edgar on 01/02/2015.
  */
 public class PruebaGit {
+    private int num1, num2, res;
+
+    private void suma(){
+        num1 = 1;
+        num2 = 2;
+
+        res = num1 + num2;
+    }
 
     public static void main(String args[]){
+        PruebaGit info = new PruebaGit();
+        info.suma();
+
         System.out.print("Hola Mundo!!!");
+        System.out.println("Resultado: " + info.res);
     }
 }
